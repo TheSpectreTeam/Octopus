984376fe42aea7ac27b5f47bf7a59a58fbdc1a76
Vladititov <capt.Hoffmann@gmail.com>
2022-04-27 16:28:58 +0300

    feature(repository)[OCT-5]: added project for base repository, added base model in common

---
+++ src/BuildingBlocks/Common/Common.Models/DomainModels/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.Models.DomainModels
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/BuildingBlocks/Repositories/Repository.Base/Abstractions/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using Common.Models.DomainModels;
+
+namespace Repository.Base.Abstractions
+{
+    public interface IRepositoryBase<T> where T : BaseEntity
+    {
+        Task CreateAsync(T item);
+        Task RemoveAsync(int id);
+        Task<T> GetAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
