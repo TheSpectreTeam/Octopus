f05f1908fa7debf8117f0cf19b1b33c33c73f3f6
kogonidze <kottok1998@gmail.com>
2022-05-14 15:18:06 +0300

    feat(parserApi)[OCT-27]: added MediatR to the parser project

--- src/Services/Parser/Api/Presentation/Parser.Presentation.WebApi/Program.cs
+++ src/Services/Parser/Api/Presentation/Parser.Presentation.WebApi/Program.cs
@@ -1,4 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
