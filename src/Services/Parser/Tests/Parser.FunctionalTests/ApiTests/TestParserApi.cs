aec9e2a7bd62baf6bd460efebdea115a8f68535d
Vasili <kottok1998@gmail.com>
2022-06-09 14:56:06 +0300

    feat(parserApi): added functional tests

---
+++ src/Services/Parser/Tests/Parser.FunctionalTests/ApiTests/TestParserApi.cs
@@ -0,0 +1,27 @@
+﻿namespace Parser.FunctionalTests.ApiTests
+{
+    public class TestParserApi : WebApplicationFactory<Program>
+    {
+        private readonly string _environment;
+        private readonly Action<IServiceCollection>? _serviceOverride;
+
+        public TestParserApi(
+            Action<IServiceCollection> serviceOverride,
+            string environment = "Development")
+        {
+            _serviceOverride = serviceOverride;
+            _environment = environment;
+        }
+
+        protected override IHost CreateHost(IHostBuilder hostBuilder)
+        {
+            if (_serviceOverride is not null)
+            {
+                hostBuilder.UseEnvironment(_environment);
+                hostBuilder.ConfigureServices(_serviceOverride);
+            }
+            return base.CreateHost(hostBuilder);
+        }
+    }
+
+}
